a2aff88c996ac3f7e6044554828e53e1c4d8ba4f
Guy Tepper <guytepper@gmail.com>
2016-11-18 10:18:42 +0200

    Added vue webpack-simple template

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  color: #2c3e50;
+}
+</style>
